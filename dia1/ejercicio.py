aad1f1c2bb7f4a1778ffd6d4cb10c266029441aa
dlarenasd <d.larenas.d@gmail.com>
2024-03-12 12:48:24 -0300

    jugando con print

--- dia1/ejercicio.py
+++ dia1/ejercicio.py
@@ -10,7 +10,7 @@ print (2+2)
 print (12-2)
 print (2*2)
 print (2/13) #las divisiones siempre darán como resultado un float, con o sin decimal
-numero = 23
+numero = 32
 print (numero)
 
 #limitantes de python
@@ -18,4 +18,17 @@ print (numero)
 #print ("texto"+13) TypeError: can only concatenate str (not "int") to str
 
 #INTERPOLACIÓN
-print(f"el numero es {numero}")
+print(f"el numero es {numero}") #la f permite interpretar dentro del print
+nombre = "Diego"
+
+print(f"tu nombre es {nombre} y tienes {numero} años")
+
+#string.format
+print("Tu nombre es {} y tu edad es {}".format(nombre,numero))
+
+# formato con %: %s para string y %d para numeros
+print("Tu nombre es %s y tu edad es %d" % (nombre,numero))
+
+#metodo con cadena /title hace que la primera letra de las palabras sea mayúscula y las demás minúscula
+apellido ="larenas dintrans"
+print(apellido.title())
