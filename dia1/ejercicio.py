54a476f97987a811c86c9d0e05ef60e61327ad0c
dlarenasd <d.larenas.d@gmail.com>
2024-03-12 12:27:42 -0300

    conociendo python

---
+++ dia1/ejercicio.py
@@ -0,0 +1,21 @@
+#esto es un comentario de una sola línea
+"""
+comentario
+de más 
+de una 
+línea
+"""
+print ("texto de prueba")
+print (2+2)
+print (12-2)
+print (2*2)
+print (2/13) #las divisiones siempre darán como resultado un float, con o sin decimal
+numero = 23
+print (numero)
+
+#limitantes de python
+#no está permitido la suma de letras y números
+#print ("texto"+13) TypeError: can only concatenate str (not "int") to str
+
+#INTERPOLACIÓN
+print(f"el numero es {numero}")
