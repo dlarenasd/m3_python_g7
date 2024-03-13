ba0e494be31a40310b81b0c5d397c65be1104763
dlarenasd <d.larenas.d@gmail.com>
2024-03-13 13:04:13 -0300

    operadores y librerias

---
+++ dia2/operadores_librerias.py
@@ -0,0 +1,23 @@
+#import math
+from math import pow,sqrt, ceil
+
+
+print(2**4) #potencia y exponente--->16
+print(5/2) #división con resultado flotante --->2.5
+print(5//2) #división con resultado solo de parte entera --->2
+print(5%2) #módulo de la división (el resto de una división)---->1
+
+"""Prioridad de operaciones (frente a igual prioridad se resulve de izqeuirda a derecha)
+1. Potencias
+2. Multiplicaciones, Divisiones y Módulo
+3. Suma y Resta
+*Si hay paréntesis eso se hace primero"""
+
+print(pow(4,2))
+print(sqrt(100))
+print(ceil(3.00001)) #aproxima al entero superior
+
+edad = int(input("ingresa tu edad"))
+
+edad = input("ingresa tu edad")
+edad = int(edad)
