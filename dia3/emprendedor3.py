5ed9bb7b3d19f6575654656c3159f8f13a321bcc
dlarenasd <d.larenas.d@gmail.com>
2024-03-14 12:24:28 -0300

    corrección a función round

--- dia3/emprendedor3.py
+++ dia3/emprendedor3.py
@@ -11,6 +11,6 @@ preuti = input("Ingresa las utilidades del año pasado \n")
 preuti = float(preuti)
 
 diferencia = utilidades - preuti
-diferencia = round(diferencia,2)
+
 print(f"Las utilidades son {utilidades} pesos")
-print(f"Y la diferencia entre las utilidades del año pasado y las actuales es de {diferencia} pesos")
+print(f"Y la diferencia entre las utilidades del año pasado y las actuales es de {round(diferencia,2)} pesos")
--- dia3/escape.py
+++ dia3/escape.py
@@ -4,6 +4,5 @@ radio = float(radio)
 constante = input("Ingresa la constante g (recuerda usar punto y no coma) \n")
 constante = float(constante)
 velocidad = (math.sqrt(2*(radio*1000)*constante))
-velocidad = round(velocidad,1)
 
-print(f"La velocidad de escape es {velocidad} [m/s]")
+print(f"La velocidad de escape es {round(velocidad,1)} [m/s]")
