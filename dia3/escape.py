b0a05aed603749ecb32609911fe2d0b9459cefbf
dlarenasd <d.larenas.d@gmail.com>
2024-03-14 12:47:10 -0300

    correciones a variables

--- dia3/escape.py
+++ dia3/escape.py
@@ -1,8 +1,8 @@
 import math
-radio = input("Ingresa el radio del planeta en Kilometros \n")
-radio = float(radio)
-constante = input("Ingresa la constante g (recuerda usar punto y no coma) \n")
-constante = float(constante)
-velocidad = (math.sqrt(2*(radio*1000)*constante))
+radioPlaneta = input("Ingresa el radio del planeta en Kilometros \n")
+radioPlaneta = float(radioPlaneta)
+constanteGravitacional = input("Ingresa la constante g (recuerda usar punto y no coma) \n")
+constanteGravitacional = float(constanteGravitacional)
+velocidadEscape = (math.sqrt(2*(radioPlaneta*1000)*constanteGravitacional))
 
-print(f"La velocidad de escape es {round(velocidad,1)} [m/s]")
+print(f"La velocidad de escape es {round(velocidadEscape,1)} [m/s]")
