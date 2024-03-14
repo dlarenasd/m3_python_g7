8d77ad6fd180ad8abd12e1ffe5074cb025f52f0b
dlarenasd <d.larenas.d@gmail.com>
2024-03-14 10:01:18 -0300

    correcciones decimales

--- dia3/emprendedor3.py
+++ dia3/emprendedor3.py
@@ -8,8 +8,9 @@ gasto = int(gasto)
 utilidades = (precio * usuarios) - gasto
 
 preuti = input("Ingresa las utilidades del año pasado \n")
-preuti = int(preuti)
+preuti = float(preuti)
 
 diferencia = utilidades - preuti
+diferencia = round(diferencia,2)
 print(f"Las utilidades son {utilidades} pesos")
 print(f"Y la diferencia entre las utilidades del año pasado y las actuales es de {diferencia} pesos")
