5b7f31378afcf6f57b34310185d239dd318988fc
dlarenasd <d.larenas.d@gmail.com>
2024-03-19 08:37:18 -0300

    algoritmos y lógica

---
+++ dia5/ejercicio5.py
@@ -0,0 +1,8 @@
+edad = 27
+#Juan es mayor de edad?
+print(edad>=18)#True
+print (edad<18)#False
+
+#Juan se graduó antes de los 18 años?
+edad_graduacion_colegio = 17
+print(edad_graduacion_colegio <18)
