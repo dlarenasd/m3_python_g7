874ee2145fa4f0f0850ecb4a5ad10bbff6deb5b8
dlarenasd <d.larenas.d@gmail.com>
2024-03-20 10:47:57 -0300

    comentarios

--- dia6/imc.py
+++ dia6/imc.py
@@ -1,17 +1,25 @@
 nombre = input("Ingrese su nombre \n")
+#BIENVENIDA
 print(f"Hola {nombre}, a continuación le pediré algunos datos para calcular su Índice de Masa Corporal(o IMC). Recuerde que este índice es solo un número y que no representa su estado de salud, por lo que debe asesorarse con un médico u otro profesional del área.")
+#INGRESO DE DATOS
 peso = float(input("Por favor ingrese su peso en kilogramos \n"))
+while peso <=0: #VALIDACIÓN
+    print("Valor de peso incorrecto")
+    peso = float(input("Por favor ingrese su peso en kilogramos \n"))
 altura = float(input("Por favor ingrese su altura en centímetros \n"))
-while altura <=0:
+while altura <=0: #VALIDACIÓN
     print("Valor de estatura incorrecto")
-    altura = int(input("Por favor ingrese su altura en centímetros \n"))
-    
+    altura = float(input("Por favor ingrese su altura en centímetros \n"))
+
+#CONVERSIÓN Y CÁLCULO    
 altura = altura/100
 
 indice_masa_corporal = peso/(altura**2)
 
+#OUTPUT
 print(f"Su índice de masa corporal es {round(indice_masa_corporal,2)}")
 
+#CONDICIONES
 if indice_masa_corporal < 18.5:
     print("Ese valor se clasifica como Bajo Peso")
 elif indice_masa_corporal >= 18.5 and indice_masa_corporal < 25:
@@ -22,5 +30,5 @@ elif indice_masa_corporal >=30 and indice_masa_corporal < 35:
     print("Ese valor se clasifica como Obesidad I")
 elif indice_masa_corporal >=35 and indice_masa_corporal < 40:
     print("Ese valor se clasifica como Obesidad II")
-else: 
+elif indice_masa_corporal >=40: 
     print("Ese valor se clasifica como Obesidad III")
--- dia6/juego.py
+++ dia6/juego.py
@@ -1,27 +1,32 @@
 from random import choice
+#BIENVENIDA
 nombre = input("Ingrese su nombre a continuación: \n")
 print(f"Hola {nombre}, este es el juego del Cachipún, donde tendrás que elegir entre tres jugadas y vencer al computador.")
+
+#DEFINICIÓN DE VARIABLES BASE
 cachipun = ["Piedra","Papel","Tijera"]
 computador=choice(cachipun)
 
+#INGRESO DE JUGADA
 usuario=input("Ingresa tu jugada (Piedra, Papel o Tijera): \n")
 usuario = usuario.title()
 
+#VALIDACIÓN
 while usuario != "Piedra" and usuario != "Papel" and usuario != "Tijera":
     print("Jugada no válida")
     usuario=input("Ingresa tu jugada (Piedra, Papel o Tijera): \n")
     usuario = usuario.title()
-
+    
+#OUTPUT
 print(f"Jugaste: {usuario}")
 print(f"El computador jugó: {computador}")
 
-if usuario == "Piedra" and computador == "Tijera":
-    print("¡GANASTE!")
-elif usuario == "Papel" and computador == "Piedra":
-    print("¡GANASTE!")
-elif usuario == "Tijera" and computador == "Papel":
-    print("¡GANASTE!")
-elif usuario == computador:
+#CONDICIONES
+if usuario == computador:
     print("¡EMPATE!")
+elif (usuario == "Piedra" and computador == "Tijera") or\
+(usuario == "Papel" and computador == "Piedra") or \
+(usuario == "Tijera" and computador == "Papel"):
+    print("¡GANASTE!")  
 else:
     print("¡PERDISTE!")
