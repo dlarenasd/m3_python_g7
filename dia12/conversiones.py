6d27b2ae878aa1d4fa33c2b60efb1d9b86f0ddeb
dlarenasd <d.larenas.d@gmail.com>
2024-03-27 14:55:26 -0300

    diccionario a la tabla de conversiones

--- dia12/conversiones.py
+++ dia12/conversiones.py
@@ -6,13 +6,14 @@ a Sol peruano: 0.0046
 
 """
 
+conversiones={
+"sol_peruano": float(sys.argv[1]),
+"peso_argentino" : float(sys.argv[2]),
+"dolar_americano" : float(sys.argv[3]),
+"peso_chileno" : int(sys.argv[4]),
+}
 
-soles_peruanos = float(sys.argv[1])
-pesos_argentinos = float(sys.argv[2])
-dolares_americanos = float(sys.argv[3])
-pesos_chilenos = float(sys.argv[4])
-
-print(f"Los {pesos_chilenos} pesos equivalen a:") 
-print(f"{soles_peruanos * pesos_chilenos} Soles")
-print(f"{pesos_argentinos * pesos_chilenos} Pesos Argentinos") 
-print(f"{dolares_americanos * pesos_chilenos} Dólares Americanos")
+print(f"Los {conversiones["peso_chileno"]} pesos equivalen a:") 
+print(f"{conversiones["sol_peruano"] * conversiones["peso_chileno"]} Soles")
+print(f"{conversiones["peso_argentino"] * conversiones['peso_chileno']} Pesos Argentinos") 
+print(f"{conversiones["dolar_americano"] * conversiones['peso_chileno']} Dólares Americanos")
