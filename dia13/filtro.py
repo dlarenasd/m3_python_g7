990090ab1ada6298cd15bcf41b9deddbc8d55e3a
dlarenasd <d.larenas.d@gmail.com>
2024-04-01 10:54:27 -0300

    tabulaciones

--- dia13/filtro.py
+++ dia13/filtro.py
@@ -31,13 +31,11 @@ def respuesta():
     else:
         return "Lo sentimos, no es una operación válida"
 
-
 precios = {'Notebook': 700000,
     'Teclado': 25000,
     'Mouse': 12000,
     'Monitor': 250000,
     'Escritorio': 135000,
     'Tarjeta de Video': 1500000}
-
 
 print(respuesta())
