3464e2a8872ea866b71bdfcaf5901a5a295a86f2
dlarenasd <d.larenas.d@gmail.com>
2024-04-04 15:11:13 -0300

    uso de funciones para los templates y comentarios

--- dia17/Prueba/__pycache__/main.py
+++ dia17/Prueba/__pycache__/main.py
@@ -1,7 +1,6 @@
 import requests
 import json 
-from string import Template
-
+import template
 def request_get(url):
     """Función para obtener información de una API
 
@@ -13,56 +12,31 @@ def request_get(url):
     """
     return json.loads(requests.get(url).text)
 
-response = request_get("https://aves.ninjas.cl/api/birds")
-#img_template = Template('<img src="$url">')
-#print(len(response)) --->216
-#print(type(response)) ---><class 'list'>
-#print(response[0])
-html_template = Template('''<!DOCTYPE html>
-<html>
-<head>
-<title>Aves de Chile</title>
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-</head>
-<body>
-<h1 class="text-center">Aves de Chile</h1>
-<div class="container">
-    <div class="row gx-3 gy-3">
-            $cards
-    </div>
-</div>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-</body>
-</html>
-''')
-card_template = Template("""<div class="col-10, col-md-4">
-            <div class="card" style="width: 18em;">
-                <img src="$url" class="card-img-top" alt="$txt_es">
-                <div class="card-body">
-                    <p class="card-text">$txt_es</p>
-                    <p class="card-text">$txt_en</p>
-                </div>
-            </div>
-        </div>""") 
+response = request_get("https://aves.ninjas.cl/api/birds") #solicito el contenido de la API con la función
 
+#print(len(response)) --->tiene 216 elementos
+#print(type(response)) ---><class 'list'>
+#print(response[0]) -->{'uid': '76-buteo-albigula', 'name': {'spanish': 'Aguilucho Chico', 'english': 'White-throated Hawk', 'latin': 'Buteo albigula'}, 'images': {'main': 'https://aves.ninjas.cl/api/site/assets/files/3099/17082018024245aguilucho_chico_tomas_rivas_web.200x0.jpg', 'full': 'https://aves.ninjas.cl/api/site/assets/files/3099/17082018024245aguilucho_chico_tomas_rivas_web.jpg', 'thumb': 'https://aves.ninjas.cl/api/site/assets/files/3099/17082018024245aguilucho_chico_tomas_rivas_web.200x0.jpg'}, '_links': {'self': 'https://aves.ninjas.cl/api/birds/76-buteo-albigula', 'parent': 'https://aves.ninjas.cl/api/birds'}, 'sort': 0}
+#cada elemento de la lista response es un diccionario
 lista_url_fotos = []
 nombre_es = []
 nombre_en = []
-for diccionario in response:
-    lista_url_fotos.append(diccionario['images']['main'])
-    nombre_es.append(diccionario['name']['spanish'])
-    nombre_en.append(diccionario['name']['english'])
-    
-cards=""    
-for foto, nom_es, nom_en in zip(lista_url_fotos,nombre_es,nombre_en):
-    
-    cards += card_template.substitute(url=foto, txt_es=nom_es, txt_en=nom_en)+"\n"
-    
+for diccionario in response: #recorro cada diccionario de la lista obtenida
+    lista_url_fotos.append(diccionario['images']['main']) #e incluyo en una lista las URL de las fotos
+    nombre_es.append(diccionario['name']['spanish']) #también armo una lista para los nombres en español
+    nombre_en.append(diccionario['name']['english']) #y una lista para los nombres en inglés
 
-html = html_template.substitute(cards = cards)
-print(html)
+card_template = template.cards_template() #hago una variable que contenga el módulo para usar los template de cards(si no lo hago no puedo usar el substitute)
+cards=""    #Hago un string vacío para luego rellenarlo
+for foto, nom_es, nom_en in zip(lista_url_fotos,nombre_es,nombre_en): #asocio en tríos url, nombre en español y nombre en inglés y los recorro
+    
+    cards += card_template.substitute(url=foto, txt_es=nom_es, txt_en=nom_en)+"\n"  #y relleno a continuación el string vacío con las 216 iteraciones/tríos en forma de card
+    
+html_template = template.html_template() #asocio una variable al template obtenido por función
+html = html_template.substitute(cards = cards) #sustituyo en el template y relleno con la variable cards que contiene las 216 cards
+#print(html)
 
-with open('output.html', 'w') as file:
+with open('index.html', 'w') as file:  #abro un archivo de nombre index.html en modo de escritura y lo relleno con el html recién hecho, luego lo exporto
     file.write(html)
 
 
--- dia17/Prueba/__pycache__/template.py
+++ dia17/Prueba/__pycache__/template.py
@@ -1,24 +1,26 @@
 from string import Template
-
-html_template = Template('''<!DOCTYPE html>
-<html>
-<head>
-<title>Aves de Chile</title>
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-</head>
-<body>
-<h1 class="text-center">Aves de Chile</h1>
-<div class="container">
-    <div class="row gx-3 gy-3">
-            $cards
+def html_template():
+    html_template = Template('''<!DOCTYPE html>
+    <html>
+    <head>
+    <title>Aves de Chile</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    </head>
+    <body>
+    <h1 class="text-center">Aves de Chile</h1>
+    <div class="container">
+        <div class="row gx-3 gy-3">
+                $cards
+        </div>
     </div>
-</div>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-</body>
-</html>
-''') #cards = html_template.substitute(cards='xxxx')
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    </body>
+    </html>
+    ''')#cards = html_template.substitute(cards='xxxx')//incluyo bootstrap para que funcione el html y dejo una estructura en fila para las cards
+    return html_template
 
-card_template = Template("""<div class="col-10, col-md-4">
+def cards_template():
+    card_template = Template("""<div class="col-10, col-md-4">
             <div class="card" style="width: 18em;">
                 <img src="$url" class="card-img-top" alt="$txt_es">
                 <div class="card-body">
@@ -26,7 +28,8 @@ card_template = Template("""<div class="col-10, col-md-4">
                     <p class="card-text">$txt_en</p>
                 </div>
             </div>
-        </div>""") #card_template.substitute(url="xxxxxxx", txt_es="xxxxxxx", txt_en="xxxxxxx")
+        </div>""") #card_template.substitute(url="xxxxxxx", txt_es="xxxxxxx", txt_en="xxxxxxx") // les dejo un div de columna para que cada card se comporte como columna (y responsiva)
+    return card_template
 
 
     
